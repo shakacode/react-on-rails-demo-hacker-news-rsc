45e7bd25d7899e6bfb70125986da8b8d31a32ea3
Justin Gordon <justin@shakacode.com>
2026-04-23 21:31:57 -1000

    Pin Puma bind host for Puma 8

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,8 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` and `host` that Puma will listen on; default is 3000 on all IPv4 interfaces.
+port ENV.fetch("PORT", 3000), ENV.fetch("PUMA_BIND_HOST", "0.0.0.0")
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
